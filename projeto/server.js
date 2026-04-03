088087bb41135d37c1f7ee023f7e025f7865e66a
vicnozz <victorhugopereiralemos6@gmail.com>
2026-04-03 19:04:04 -0300

    Update server.js
    apenas ajustei para o site funcionar

--- projeto/server.js
+++ projeto/server.js
@@ -1,7 +1,7 @@
 const express =  require("express");
 const app = express();
 
-app.use(express.static("public"));
+app.use(express.static("projeto/public"));
 
 app.listen(3000, () => {
     console.log("Servidor rodando em http://localhost:3000");
